bfc387b9435ade4d4fe14d8fed333747205c69be
CD <mrrash91@gmail.com>
2020-10-28 01:03:50 +0800

    Adding tailwind css implementation

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="text-center">
+      <header className="pointer-events-none" style={{ height: '40vmin' }}>
+        <img
+          src={logo}
+          className="flex flex-col items-center justify-center bg-blue-800 text-white text-bas relative"
+          alt="logo"
+        />
+        <div className="absolute bottom-0 left-0 right-0">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="text-blue-300"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </div>
       </header>
     </div>
   );
